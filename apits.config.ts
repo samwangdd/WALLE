1a8bda98e304c929e3634ebf9ab6e3812fc43de9
samwangdd <wyysammore@gmail.com>
2023-01-16 11:02:42 +0800

    build: config

--- apits.config.ts
+++ apits.config.ts
@@ -5,21 +5,13 @@ export default defineConfig({
   serverUrl: "http://yapi.corp.hongsong.club/",
   projects: [
     {
-      token: 'af4ce26fc88a211fcb1ddd8b96dc69bc9ebef586ee793eb290cd37339b867659', // yapi项目的token
-      categories: [{ id: 7616 }], // yapi项目的分类
-    },
-    {
       token: 'fea472ac89837c7d39c9910915838a83c8b7dcc72e05596ad5a3a44a4f3dadb6',
       gatewayPrefix: '/gateway/api/user',
-      includeInterfaceProps: 'data',
+      assignInterfaceProps: 'data',
       categories: [{ id: 7644, }],
-    },
-    {
-      token: 'd93262cf7aa3bc578c090e52a3bb9b449075bf59c16a472875035e6dcf2bacb0',
-      categories: [{ id: 7106 }],
     },
   ],
   outputFilePath: 'src/api',
-  defaultRequestLib: false,
-  topImportPkgTemplate: () => `import request from '@/service/api';`,
+  // defaultRequestLib: false,
+  // topImportPkgTemplate: () => `import request from '@/service/api';`,
 });
