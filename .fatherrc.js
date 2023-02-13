ddc8df9668bba7e67d78e2fe50440e09260d27ad
samwangdd <wyysammore@gmail.com>
2023-02-13 16:40:57 +0800

    build: support esm

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,8 +1,5 @@
 export default {
   entry: 'src/index.ts',
   cjs: {type: 'babel', file: 'lib/cli'},
-  doc: {
-    themeConfig: { mode: 'dark' },
-    base: '/your-repo'
-  },
+  esm: {type: 'babel', file: 'es/cli'},
 }
